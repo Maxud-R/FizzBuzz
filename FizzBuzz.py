86409b08cfe2a894af63d3784fb7119338da06cb
Maxud-R <89140684+Maxud-R@users.noreply.github.com>
2021-08-19 20:17:01 +0300

    Edited file for work on task
    Renamed file to task name and edited class for this

---
+++ FizzBuzz.py
@@ -0,0 +1,6 @@
+class FizzBuzzDetector:
+    InputString = ''
+    def __init__(self):
+        pass
+    def getOverlappings():
+        pass
--- hw.py
+++
@@ -1,8 +0,0 @@
-class Hw:
-    JJ = 'string'
-    def __init__(self, jj):
-        self.JJ = jj
-    def print(self, s):
-        print(self.JJ+str(s))
-    def show(self):
-        print(self.JJ)
